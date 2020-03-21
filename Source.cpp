64e10fbdd36781cae3d51ad2eea28dc3e692c684
idaspahiuu <58752918+idaspahiuu@users.noreply.github.com>
2020-03-22 00:02:40 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,25 +2,75 @@
 #include<string>
 using namespace std;
 string mesazhi, celesi;
+//funksioni per kthimin e tekstit ne shkronja te medha
+void caps()
+{
+	string msg, cel, cel1;
+
+	cout << "\nShkruani tekstin qe doni te enkriptoni: "
+		cin >> msg;
+	for (int i = 0; i < msg.length(); i++)
+	{
+		msg[i] = toupper(msg[i]);
+	}
+
+	cout << "\nShkruani celesin qe doni te shfrytezoni: ";
+	cin >> cel;
+
+	for (int i = 0; i < cel.length(); i++)
+	{
+		cel[i] = toupper(cel[i]);
+	}
+
+
+	for (int i = 0; j=0; i < msg.length(); i++)
+	{
+		if (msg[i]==32)
+		{
+			cel1 = cel1 + 32;
+		}
+		else {
+			if(j<cel.length())
+			{
+				cel1 = cel1 + cel[j];
+				j++;
+			}
+			else
+			{
+				j = 0;
+				cel1 = cel1 + cel[j];
+				j++;
+
+			}//if-else(2)
+		}//if-else(1)
+
+	}//unaza for
+
+	cout << msg << "\n" << cel1;
+	mesazhi = msg;
+	celesi = cel1;
+};
 int main()
 {
-	int zgj;
+	char zgj;
 
 	cout << "Verejtje!" << endl << "Permbajtja e mesazhit dhe celesit duhet te jene tekstuale." << endl;
-	cout << "Shtypni 1 per enkriptim." << endl << "Shtypni 2 per dekriptim" << endl;
+	cout << "Shtypni 'e' per enkriptim." << endl << "Shtypni 'd' per dekriptim" << endl;
 	cout << "Zgjedhja: ";
 	cin >> zgj;
-
-	switch (zgj)
+	
+	if (zgj == 'e')
 	{
-	case 1:
-		cout << "Enkriptimi: ";
-		break;
-	case 2:
-		cout << "Dekriptimi: ";
-		break;
-	default:
-		break;
+		cout << "Enkriptimi: " << endl;
+		caps();
+	}
+	else if (zgj == 'd')
+	{
+		cout << "Dekriptimi: " << endl;
+	}
+	else
+	{
+		cout << "\nZgjedhja e padefinuar!";
 	}
 
 	return 0;
