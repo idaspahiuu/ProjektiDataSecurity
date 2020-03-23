bdddcf44cc53f19514d6c4dc1b5274cf5f08d3bb
idaspahiuu <58752918+idaspahiuu@users.noreply.github.com>
2020-03-23 18:37:23 +0100

    Update Source.cpp
    kodi perfundimtar

--- Source.cpp
+++ Source.cpp
@@ -1,77 +1,164 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+
 using namespace std;
+
 string mesazhi, celesi;
-//funksioni per kthimin e tekstit ne shkronja te medha
-void caps()
-{
-	string msg, cel, cel1;
+//funksioni per kthimin e tekstit dhe celesit ne shkronja te medha si dhe pershatja e celesit me tekstin(per nga madhesia)
+void caps() {
+    string msg;
+    cout << "Shkruani tekstin qe doni te enkriptoni/dekriptoni: ";
+    getline(cin, msg);
+    cin.ignore();
 
-	cout << "\nShkruani tekstin qe doni te enkriptoni: "
-		cin >> msg;
-	for (int i = 0; i < msg.length(); i++)
-	{
-		msg[i] = toupper(msg[i]);
-	}
+    for (int i = 0; i < msg.length(); i++) {
+        msg[i] = toupper(msg[i]);
+    }
 
-	cout << "\nShkruani celesin qe doni te shfrytezoni: ";
-	cin >> cel;
-
-	for (int i = 0; i < cel.length(); i++)
-	{
-		cel[i] = toupper(cel[i]);
-	}
+    string cel;
+    cout << "Shkruani celesin per enkriptim/dekriptim: ";
+    getline(cin, cel);
+    cin.ignore();
 
 
-	for (int i = 0; j=0; i < msg.length(); i++)
-	{
-		if (msg[i]==32)
-		{
-			cel1 = cel1 + 32;
-		}
-		else {
-			if(j<cel.length())
-			{
-				cel1 = cel1 + cel[j];
-				j++;
-			}
-			else
-			{
-				j = 0;
-				cel1 = cel1 + cel[j];
-				j++;
+    for (int i = 0; i < cel.length(); i++) {
+        cel[i] = toupper(cel[i]);
+    }
 
-			}//if-else(2)
-		}//if-else(1)
 
-	}//unaza for
+    string cel1 = "";
+    for (int i = 0, j = 0; i < msg.length(); i++) {
+        if (msg[i] == 32) {
+            cel1 += 32;
+        }
+        else {
+            if (j < cel.length()) {
+                cel1 += cel[j];
+                j++;
+            }
+            else {
+                j = 0;
+                cel1 += cel[j];
+                j++;
+            }//ifelse(2)
+        } //ifelse(1)
+    } //unaza for
 
-	cout << msg << "\n" << cel1;
-	mesazhi = msg;
-	celesi = cel1;
-};
+
+    mesazhi = msg;
+    celesi = cel1;
+}
+
+int tabela[26][26];
+void krijimiTabeles() {
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < 26; j++) {
+            int n;
+            if ((i + 65) + j > 90) {
+                n = ((i + 65) + j) - 26;
+
+                //ASCII kodi i shkronjes se alfabetit ne poziten e indeksit te tabeles
+
+                tabela[i][j] = n;
+            }
+            else {
+                n = (i + 65) + j;
+
+
+                tabela[i][j] = n;
+            }
+        }
+    }
+
+}
+//Krijimi i funksionit per enkriptim
+void enkriptimi(string mesazhi, string celesi) {
+    krijimiTabeles();
+    string ciphertxt = "";
+    for (int i = 0; i < mesazhi.length(); i++) {
+        if (mesazhi[i] == 32 && celesi[i] == 32) {
+            ciphertxt += " ";
+        }
+        else {
+            int x = (int)mesazhi[i] - 65;
+            int y = (int)celesi[i] - 65;
+            ciphertxt += (char)tabela[x][y];
+        }
+    }
+
+    cout << "Teksti i enkriptuar: " << ciphertxt;
+}
+
+
+//fillojme numerimin nga ASCII kodi per shkronjen e celesit dhe mbarojme tek shkronja e fundit e mesazhit
+int numerimi(int cel, int msg) {
+    int k = 0;
+    string rez = "";
+
+    //qe ti perfshjime te gjitha shkronjat e alfabetit(26) kemi:
+    for (int i = 0; i < 26; i++) {
+        if (cel + i > 90) {
+            rez += (char)(cel + (i - 26));
+        }
+        else {
+            rez += (char)(cel + i);
+        }
+    }
+
+    for (int i = 0; i < rez.length(); i++) {
+        if (rez[i] == msg) {
+            break;
+        }
+        else {
+            k++;
+        }
+    }
+    return k;
+}
+
+//Krijimi i funksionit per dekriptim
+void dekriptimi(string mesazhi, string celesi) {
+    string plaintxt = "";
+    for (int i = 0; i < mesazhi.length(); i++) {
+        if (mesazhi[i] == 32 && celesi[i] == 32) {
+            plaintxt += " ";
+        }
+        else {
+            int n = numerimi((int)celesi[i], (int)mesazhi[i]);
+            plaintxt += (char)(65 + n);
+        }
+    }
+
+    cout << "Teksti i dekriptuar: " << plaintxt;
+}
+
 int main()
 {
-	char zgj;
+    cout << "Verejte!" << endl;
+    cout << "Mesazhi dhe teksti duhet patjeter te jene me permbajtje alfabetike, nuk pranohen numra dhe karaktere te tjera!";
+    char zgj;
+    cout << "\n\nShtypni 'e' per Enkriptim.";
+    cout << "\nShtypni 'd' per Dekriptim.";
+    cout << "\nShkruani zgjedhjen tuaj ketu : ";
+    cin >> zgj;
+    cin.ignore();
 
-	cout << "Verejtje!" << endl << "Permbajtja e mesazhit dhe celesit duhet te jene tekstuale." << endl;
-	cout << "Shtypni 'e' per enkriptim." << endl << "Shtypni 'd' per dekriptim" << endl;
-	cout << "Zgjedhja: ";
-	cin >> zgj;
-	
-	if (zgj == 'e')
-	{
-		cout << "Enkriptimi: " << endl;
-		caps();
-	}
-	else if (zgj == 'd')
-	{
-		cout << "Dekriptimi: " << endl;
-	}
-	else
-	{
-		cout << "\nZgjedhja e padefinuar!";
-	}
+    switch (zgj)
+    {
+    case 'e':
+        cout << "Enkriptimi: " << endl;
+        caps();
+        enkriptimi(mesazhi, celesi);
+        break;
+    case 'd':
+        cout << "Dekriptimi: " << endl;
+        caps();
+        dekriptimi(mesazhi, celesi);
+        break;
+    default:
+        cout << "Zgjedhja e padefinuar!" << endl;
+        break;
+    }
 
-	return 0;
+    return 0;
 }
