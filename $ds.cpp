83debe927fafaab7f31959e55ebf523aba8fceb8
idaspahiuu <58752918+idaspahiuu@users.noreply.github.com>
2020-03-30 17:22:05 +0200

    Perfundimi i kodit
    Per shkak  se ne moment te fundit eshte dashur te nderrohet kodi, per shkak se kodet qe ne kishim implementuar me heret nuk funksiononin me ane te argumenteve, eshte vonuar dorezimi i detyres.

--- $ds.cpp
+++ $ds.cpp
@@ -120,8 +120,156 @@ public:
     }
 };
 
+class Rail {
+public:
+    string enkriptimi(string mesazhi, int rreshti)
+    {
+        int l = mesazhi.length();
+        int k = 0, kolona;
+        kolona = l / rreshti;
+
+
+        //krijimi i nje loop if i cili e kontrollon a eshte numri i kolones 
+        //numer i plote , nese nuk eshte duhet shtuar nje kolon ekstra
+        if (l % rreshti == !0)
+        {
+            kolona++;
+        }
+
+
+        // matrica s
+        char s[rreshti][kolona];
+        // futja e shkronjave ne matrice 
+        for (int i = 0; i < kolona; i++) {
+            for (int j = 0; j < rreshti; j++) {
+                s[i][j] = mesazhi[k];
+                k++;
+            }
+        }
+
+        //paraqitja e matrices se formuar
+        for (int i = 0; i < rreshti; i++) {
+            for (int j = 0; j < kolona; j++) {
+                if (s[j][i] == '\0')
+                    cout << 'X';        //kur nuk ka shkronja matrica te mbushet me shkronjen 'X'
+                cout << s[j][i];    //paraqitja matrices
+            }
+
+        }
+        cout << endl; //cout eshte per te larguar per nje rresht funksionet
+        return"";
+    }
+    string show1(string mesazhi, int rreshti) {
+        //inicializimi i l,k,rreshtit dhe kolones
+        int l = mesazhi.length();
+        int  kolona;
+        int k = 0;
+        kolona = l / rreshti;
+
+
+        //krijimi i nje loop if i cili e kontrollon a eshte numri i kolones 
+        //numer i plote , nese nuk eshte duhet shtuar nje kolon ekstra
+        if (l % rreshti != 0)
+        {
+            kolona++;
+        }
+
+
+        // matrica s
+        char s[rreshti][kolona];
+        // futja e shkronjave ne matrice 
+        for (int i = 0; i < kolona; i++) {
+            for (int j = 0; j < rreshti; j++) {
+                s[i][j] = mesazhi[k];
+                k++;
+            }
+        }
+
+        //paraqitja e matrices se formuar
+        for (int i = 0; i < rreshti; i++) {
+            for (int j = 0; j < kolona; j++) {
+                if (s[j][i] == '\0')
+                    cout << 'X';        //kur nuk ka shkronja matrica te mbushet me shkronjen 'X'
+                cout << s[j][i];    //paraqitja matrices
+            }
+            cout << endl; //cout e mundson shfaqjen e tekstit ne shirita
+        }
+        return"";
+    }
+
+    //funksioni per enkriptimin e mesazhit ne formen
+    //rail-fence
+    string dekriptimi(string mesazhi, int kolona)
+    {
+        int l = mesazhi.length();
+        int k = 0, rreshti;
+        // e kemi ba ndrrimin e kolones me rreshtin nkrahasim me kodin per enkriptimin
+        rreshti = l / kolona; // per arsye se duhet me u vendos e kunderta per me arrit dekriptimin
+
+
+
+
+        //nkrahasim me enkriptim ktu nuk kemi nevoj per krijimin e if , per aryse se matrica eshte e mbushur
+
+
+        // matrica s
+        char s[rreshti][kolona];
+        // futja e shkronjave ne matrice 
+        for (int i = 0; i < kolona; i++) {
+            for (int j = 0; j < rreshti; j++) {
+                s[i][j] = mesazhi[k];
+                k++;
+            }
+        }
+
+        //paraqitja e matrices se formuar
+        for (int i = 0; i < rreshti; i++) {
+            for (int j = 0; j < kolona; j++) {
+
+                cout << s[j][i];    //paraqitja matrices
+            }
+
+        }
+        cout << endl;
+        return "";
+    }
+    string show(string mesazhi, int kolona) {
+        int l = mesazhi.length();
+        int k = 0, rreshti;
+        // e kemi ba ndrrimin e kolones me rreshtin nkrahasim me kodin per enkriptimin
+        rreshti = l / kolona; // per arsye se duhet me u vendos e kunderta per me arrit dekriptimin
+
+
+
+
+        //nkrahasim me enkriptim ktu nuk kemi nevoj per krijimin e if , per aryse se matrica eshte e mbushur
+
+
+        // matrica s
+        char s[rreshti][kolona];
+        // futja e shkronjave ne matrice 
+        for (int i = 0; i < kolona; i++) {
+            for (int j = 0; j < rreshti; j++) {
+                s[i][j] = mesazhi[k];
+                k++;
+            }
+        }
+
+        //paraqitja e matrices se formuar
+        for (int i = 0; i < rreshti; i++) {
+            for (int j = 0; j < kolona; j++) {
+
+                cout << s[j][i];    //paraqitja matrices
+            }
+            cout << endl;
+        }
+        return "";
+    }
+};
+
 int main(int argc, char* argv[])
 {
+    Rail r;
     Caesar c;
     string key = argv[3];
     Vigenere cipher(key);
@@ -139,18 +287,37 @@ int main(int argc, char* argv[])
         }
     }
     if (strcmp(argv[1], "caesar") == 0) {
-        if (strcmp(argv[2], "encode") == 0) {
+        if (strcmp(argv[2], "encrypt") == 0) {
             string message = argv[3];
             int key = atoi(argv[4]);
             string encoded = c.encode(message, key);
             cout << encoded << endl;
         }
-        else if (strcmp(argv[2], "decode") == 0) {
+        else if (strcmp(argv[2], "decrypt") == 0) {
             string message = argv[3];
             int key = atoi(argv[4]);
             string decoded = c.decode(message, key);
             cout << decoded << endl;
 
         }
+    }
+    if (strcmp(argv[1], "rail-fence") == 0) {
+        if (strcmp(argv[2], "encrypt") == 0) {
+            string mesazhi = argv[3];
+            int kolona = atoi(argv[4]);
+            string encrypt = r.enkriptimi(mesazhi, kolona);
+            r.show1(mesazhi, kolona);
+            cout << encrypt << endl;
+
+        }
+        else if (strcmp(argv[2], "decrypt") == 0) {
+            string mesazhi = argv[3];
+            int kolona = atoi(argv[4]);
+            string decrypt = r.dekriptimi(mesazhi, kolona);
+            r.show(mesazhi, kolona);
+            cout << decrypt << endl;
+
+        }
+
     }
 }
